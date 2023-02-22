c9e39d5c31d764554c40f78202b8a5f3374e61a1
mdaushi <firdaus.ex19@gmail.com>
2023-02-23 06:38:22 +0700

    add log plugin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Spatie\Permission\Models\Role;
 use Illuminate\Support\ServiceProvider;
+use Saade\FilamentLaravelLog\Pages\ViewLog;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ViewLog::can(function (User $user) {
+            return auth()->user()->getRoleNames()[0] === 'admin';
+        });
     }
 }
