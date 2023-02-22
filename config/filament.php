908b07791d01e25933dab6ce3ffa94c15acd6b6d
mdaushi <firdaus.ex19@gmail.com>
2023-02-22 22:42:28 +0700

    add breezy

--- config/filament.php
+++ config/filament.php
@@ -61,7 +61,7 @@ return [
     |
     */
 
-    'home_url' => '/',
+    'home_url' => '/admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -84,10 +84,11 @@ return [
     |
     */
 
-    'auth' => [
-        'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
-        'pages' => [
-            'login' => \Filament\Http\Livewire\Auth\Login::class,
+    "auth" => [
+        "guard" => env("FILAMENT_AUTH_GUARD", "web"),
+        "pages" => [
+            "login" =>
+                \JeffGreco13\FilamentBreezy\Http\Livewire\Auth\Login::class,
         ],
     ],
 
@@ -316,6 +317,7 @@ return [
     'middleware' => [
         'auth' => [
             Authenticate::class,
+            'verified'
         ],
         'base' => [
             EncryptCookies::class,
