2db43d93bbba26795aaf185b75bbce7ee5b492ab
mdaushi <firdaus.ex19@gmail.com>
2023-05-17 16:46:00 +0700

    add export pd

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -2,17 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Exports\PDExport;
-use App\Models\PengajuanPerjalananDinas;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Maatwebsite\Excel\Facades\Excel;
+use App\Models\PengajuanPerjalananDinas;
+use Carbon\Carbon;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class ExportController extends Controller
 {
     public function export(Request $request)
     {
-        $datas = PengajuanPerjalananDinas::find($request->id);
-        $nama = explode(' / ', $datas->nama)[0];
-        return Excel::download(new PDExport($datas), $nama . '.xlsx', \Maatwebsite\Excel\Excel::XLSX);
+        $pengajuan = PengajuanPerjalananDinas::find($request->id);
+        $approval1 = User::where('email', $pengajuan->pegawai->approval1)->first();
+        $approval2 = User::where('email', $pengajuan->pegawai->approval2)->first();
+        $approval3 = User::where('email', $pengajuan->pegawai->approval3)->first();
+
+        $sign1 = '';
+        $sign2 = '';
+        $sign3 = '';
+        $singuser = base64_encode(QrCode::generate('dokument ini terverifikasi secara resmi di astra app. dibuat oleh ' . $pengajuan->pegawai->nama . ' pada tanggal ' . Carbon::parse($pengajuan->created_at)->format('d M Y')));
+
+        if($pengajuan->sign_chief_at){
+            $sign1 = base64_encode(QrCode::generate($this->scanQRCodeResult($pengajuan->nama_chief_signed, $pengajuan->sign_chief_at)));
+        }
+        if($pengajuan->sign_hrd_at){
+            $sign2 = base64_encode(QrCode::generate($this->scanQRCodeResult($pengajuan->nama_hrd_signed, $pengajuan->sign_hrd_at)));
+        }
+        if($pengajuan->sign_ga_at){
+            $sign3 = base64_encode(QrCode::generate($this->scanQRCodeResult($pengajuan->nama_ga_signed, $pengajuan->sign_ga_at)));
+        }
+
+        // return $approval1;
+        $pdf = Pdf::loadView('export.print', 
+        [
+            'datas' => $pengajuan, 
+            'approval1' => $approval1, 
+            'approval2' => $approval2, 
+            'approval3' => $approval3,
+            'signuser' => $singuser,
+            'sign1' => $sign1,
+            'sign2' => $sign2,
+            'sign3' => $sign3,
+        ])->setPaper('a5', 'landscape');
+        
+        return $pdf->download($pengajuan->pegawai->nama . '-' . $pengajuan->created_at . '.pdf');
+    }
+
+    private function scanQRCodeResult($signer, $date)
+    {
+        return 'dokument ini terverifikasi secara resmi di astra app. di tanda tangani oleh ' . $signer . ' pada tanggal ' . Carbon::parse($date)->format('d M Y');        
     }
 }
---
+++ resources/views/export/print.blade.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PD {{ $datas->pegawai->nama }} - {{ $datas->created_at }}</title>
+    <style type="text/css" media="all">
+        * {
+            font-size: 11px;
+            margin: 2px 2px;
+        }
+
+        table {
+            border: 1px solid;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th,
+        td {
+            padding-left: 15px;
+            padding-right: 15px;
+            padding-top: 2px;
+            padding-bottom: 2px;
+            text-align: left;
+        }
+
+        .table-header td {
+            background-color: #D9D9D9
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .uppercase {
+            text-transform: uppercase
+        }
+
+        .capitalize {
+            text-transform: capitalize
+        }
+
+        .bold {
+            font-weight: bold
+        }
+
+        .no-border {
+            border: none
+        }
+    </style>
+</head>
+
+<body>
+
+    {{-- indentity --}}
+    <table class="no-border">
+        <tr>
+            <td>
+                <p>PT ASTRA INTERNATIONAL Tbk - HONDA</p>
+                <p>CABANG : POLMAS</p>
+            </td>
+            <td>
+                <img style="float:right;width:150px" src="{{ url('/img/Gambar1.png') }}" alt="">
+            </td>
+        </tr>
+    </table>
+
+    {{-- nomor surat --}}
+    <p class="uppercase bold text-center" style="margin-top: 20px; font-size: 15px">formulis perjalanan dinas</p>
+    <p class="uppercase bold text-center" style="font-size: 15px;margin-bottom: 10px">No. : PLMS/MKS/ /IV/23</p>
+    {{-- informasi head --}}
+    <div style="overflow-x:auto;">
+        <p>Yang melakukan perjalanan dinas</p>
+        <table>
+            <tr>
+                <td style="width: 10px">Nama/NPK</td>
+                <td style="width: 2px">:</td>
+                <td style="width: 40%">{{ $datas->nama }}</td>
+                <td style="border-left: 1px solid">Prepayment</td>
+                <td>Rp -</td>
+            </tr>
+            <tr>
+                <td>Golongan</td>
+                <td>:</td>
+                <td>{{ $datas->pegawai->golongan->nama }}</td>
+                <td style="border-left: 1px solid">Pembayaran Prep</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Penginapan</td>
+                <td>:</td>
+                <td class="capitalize">{{ $datas->penginapan }}</td>
+                <td style="border-left: 1px solid"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Nama Bank :</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td style="border-left: 1px solid"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; No. Rekening :</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+    {{-- kegiatan --}}
+    <div style="overflow-x:auto;">
+        <p class="uppercase bold">Kegiatan & tiket</p>
+        <table border="1">
+            {{-- header --}}
+            <tr class="table-header">
+                <td class="text-center" rowspan="2">Tanggal</td>
+                <td class="text-center" colspan="2">Kota Tujuan</td>
+                <td class="text-center" rowspan="2">Kegiatan Pokok (Ringkas & Jelas)</td>
+                <td class="text-center" colspan="2">Jam</td>
+                <td class="text-center" rowspan="2">Maskapai / <br> No.Penerbangan</td>
+                <td class="text-center" rowspan="2">Beli Sendiri / <br> Via GA</td>
+            </tr>
+            <tr class="table-header">
+                <td class="text-center">Dari</td>
+                <td class="text-center">Ke</td>
+                <td class="text-center">Berangkat</td>
+                <td class="text-center">Tiba</td>
+            </tr>
+
+            {{-- item kegiatan --}}
+
+            @for ($i = 0; $i < 5; $i++)
+                <tr>
+                    <td style="height: 15px">{{ $datas->kegiatan[$i]->tanggal ?? ' ' }}</td>
+                    <td class="capitalize">{{ $datas->kegiatan[$i]->dari_kota ?? ' ' }}</td>
+                    <td class="capitalize">{{ $datas->kegiatan[$i]->ke_kota ?? ' ' }}</td>
+                    <td class="capitalize">{{ $datas->kegiatan[$i]->kegiatan_pokok ?? ' ' }}</td>
+                    <td>{{ $datas->kegiatan[$i]->berangkat_jam ?? '' }}</td>
+                    <td>{{ $datas->kegiatan[$i]->tiba_jam ?? '' }}</td>
+                    <td class="capitalize">{{ $datas->kegiatan[$i]->maskapai ?? ' ' }}</td>
+                    <td class="uppercase">{{ $datas->kegiatan[$i]->payment_via ?? ' ' }}</td>
+                </tr>
+            @endfor
+
+            {{-- ttd --}}
+            <tr>
+                <td class="text-center" colspan="2">Karyawan yang akan PD</td>
+                <td class="text-center" colspan="2">Menyetujui</td>
+                <td class="text-center" colspan="2">Mengetahui</td>
+                <td class="text-center" colspan="2">Mengetahui</td>
+            </tr>
+            <tr>
+                <td class="text-center" colspan="2">
+                    <p>Tanggal: {{ Carbon\Carbon::parse($datas->created_at)->format('d M Y') }}</p>
+                    <img src="data:image/png;base64, {{ $signuser }}" alt="">
+                    <p class="uppercase">({{ $datas->pegawai->nama }})</p>
+                </td>
+                <td class="text-center" colspan="2">
+                    <p>Chief/Dept Head/ <br> Region Head</p>
+                    <img src="data:image/png;base64, {{ $sign1 }}" alt="">
+                    <p class="uppercase">({{ $approval1->name ?? '-' }})</p>
+                </td>
+                <td class="text-center" colspan="2">
+                    <p>ADH</p>
+                    <img src="data:image/png;base64, {{ $sign2 }}" alt="">
+                    <p class="uppercase">({{ $approval2->name ?? '' }})</p>
+                </td>
+                <td class="text-center" colspan="2">
+                    <p>General Affair</p>
+                    <img src="data:image/png;base64, {{ $sign3 }}" alt="">
+                    <p class="uppercase">({{ $approval3->name ?? '' }})</p>
+                </td>
+            </tr>
+        </table>
+        <span>Prepayment PD harus diselesaikan 7 hari setelah kembali</span>
+
+    </div>
+
+</body>
+
+
+</html>
