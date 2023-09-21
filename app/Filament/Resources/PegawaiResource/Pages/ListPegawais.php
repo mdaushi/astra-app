490450942411fceea5766ba00daf4bb2cfd61e7e
mdaushi <firdaus.ex19@gmail.com>
2023-09-21 23:21:10 +0800

    refactor: :recycle: import pegawai

--- app/Filament/Resources/PegawaiResource/Pages/ListPegawais.php
+++ app/Filament/Resources/PegawaiResource/Pages/ListPegawais.php
@@ -88,51 +88,25 @@ class ListPegawais extends ListRecords
                 ], columns: 2)
                 ->handleRecordCreation(function ($data) {
 
-                    // record golongan
                     $golongan = $this->handleGolonganCreation($data);
 
-                    // // record jabatan
                     $jabatan = $this->handleJabatanCreation($data);
 
+                    $pegawai = $this->createOrUpdate($data, $golongan->id, $jabatan);
 
-                    // roles string to array
-                    $rolesArray = explode(',', $data['role']);
-                    $rolesArrayCollect = collect();
-
-                    foreach ($rolesArray as $role) {
-                        $role = trim($role);
-                        $rolesArrayCollect->push($role);
-                    }
-
-                    // record akun
-                    $user = User::create([
-                        'name' => $data['nama'],
-                        'email' => $data['email'],
-                        'email_verified_at' => Carbon::now(),
-                        'password' => Hash::make('1234567890')
-                    ])->assignRole($rolesArrayCollect);
-
-                    $pegawai = Pegawai::create([
-                        'nama' => $data['nama'],
-                        'npk' => $data['npk'],
-                        'golongan_id' => $golongan->id,
-                        'kode_area' => $data['kode_area'],
-                        'area' => $data['area'],
-                        'user_id' => $user->id,
-                        'is_faktur_ekspedisi' => $data['is_faktur_ekspedisi'],
-                        'approval1' => $data['approval1'],
-                        'approval2' => $data['approval2'],
-                        'approval3' => $data['approval3'],
-                        'whatsapp' => $data['whatsapp']
-                    ]);
-
-                    $pegawai->jabatan()->sync($jabatan);
                     return $pegawai;
                 })
         ];
     }
 
-    private function handleGolonganCreation($data)
+    /**
+     * 
+     * created golongan
+     * 
+     * @param  array  $data
+     * @return  Golongan
+     */
+    private function handleGolonganCreation($data): Golongan
     {
         $query = Golongan::query();
         $golongan = $query->where('nama', $data['nama_golongan'])->first();
@@ -147,7 +121,14 @@ class ListPegawais extends ListRecords
         return $golongan;
     }
 
-    private function handleJabatanCreation($data)
+     /**
+     * 
+     * created jabatan
+     * 
+     * @param  array   $data
+     * @return  array   jabatan's id
+     */
+    private function handleJabatanCreation($data): array
     {
         $data = $data['nama_jabatan'];
 
@@ -168,39 +149,192 @@ class ListPegawais extends ListRecords
             $jabatanIdArray->push($jabatan->id);
         }
 
-        return $jabatanIdArray;
+        return $jabatanIdArray->toArray();
     }
 
-    private function handlePegawaiCreation($data, $golongan, $jabatan)
+    /**
+     * 
+     * checking account exists
+     * 
+     * @param  string  $email
+     * @return bool
+     */
+    private function isAccountExists($npk): bool
+    {
+        return ($this->getPegawai($npk))->user()->exists();
+    }
+
+    /**
+     * 
+     * checking pegawai exists
+     * 
+     * @param  string  $npk
+     * @return bool
+     */
+    private function isPegawaiExists($npk): bool
+    {
+        return Pegawai::where('npk', $npk)->exists();
+    }
+
+     /**
+     * 
+     * get account user
+     * 
+     * @param  string  $email
+     * @return  User
+     */
+    private function getAccount($npk): User
+    {
+        return ($this->getPegawai($npk))->user;
+    }
+
+    /**
+     * 
+     * get pegawai user
+     * 
+     * @param  string  $npk
+     * @return  Pegawai 
+     * 
+     */
+    private function getPegawai($npk): Pegawai
+    {
+        return Pegawai::where('npk', $npk)->first();
+    }
+
+    /**
+     * create pegawai or update if exists
+     * 
+     * @param  array  $data
+     * @return  Pegawai
+     */
+    private function createOrUpdate(array $data, int $golonganId, array $jabatan): Pegawai
+    {
+        if($this->isPegawaiExists($data['npk']))
+        {
+            // updated
+            return $this->handlePegawaiUpdated($data, $golonganId, $jabatan);
+        }
+        // created
+        return $this->handlePegawaiCreation($data, $golonganId, $jabatan);
+    }
+
+    /**
+     * create pegawai
+     * 
+     * @param  array  $data
+     * @param  int $golonganId
+     * @param  array $jabatan
+     * 
+     * @return  Pegawai
+     */
+    private function handlePegawaiCreation(array $data, int $golonganId, array $jabatan): Pegawai
     {
         $user = $this->handleUserCreation($data);
-        return Pegawai::updateOrCreate(
-            ['npk', $data['npk']],
-            [
-                'nama' => $data['nama'],
-                // 'npk' => $data['npk'],
-                'golongan_id' => $golongan->id,
-                'jabatan_id' => $jabatan->id,
-                'kode_area' => $data['kode_area'],
-                'area' => $data['area'],
-                'user_id' => $user->id,
-                'is_faktur_ekspedisi' => $data['is_faktur_ekspedisi'],
-                'approval1' => $data['approval1'],
-                'approval2' => $data['approval2'],
-                'approval3' => $data['approval3']
-            ]
-        );
+
+        $pegawai = Pegawai::create([
+            'nama' => $data['nama'],
+            'npk' => $data['npk'],
+            'golongan_id' => $golonganId,
+            'kode_area' => $data['kode_area'],
+            'area' => $data['area'],
+            'user_id' => $user->id,
+            'is_faktur_ekspedisi' => $data['is_faktur_ekspedisi'],
+            'approval1' => $data['approval1'],
+            'approval2' => $data['approval2'],
+            'approval3' => $data['approval3'],
+            'whatsapp' => $data['whatsapp']
+        ]);
+
+        $pegawai->jabatan()->sync($jabatan);
+        return $pegawai;
     }
 
-    private function handleUserCreation($data)
+    /**
+     * create user account
+     * 
+     * @param  array $data
+     * @return  User
+     */
+    private function handleUserCreation(array $data): User
     {
-        $user = User::create([
+        $rolesArrayCollect = $this->rolesStringToArray($data['role']);
+
+        return User::create([
             'name' => $data['nama'],
             'email' => $data['email'],
             'email_verified_at' => Carbon::now(),
             'password' => Hash::make('1234567890')
-        ])->assignRole($data['role']);
+        ])->assignRole($rolesArrayCollect);
+    }
+
+    /**
+     * update pegawai
+     * 
+     * @param  array  $data
+     * @param  int $golonganId
+     * @param  array $jabatan
+     * 
+     * @return  Pegawai
+     */
+    private function handlePegawaiUpdated(array $data, int $golonganId, array $jabatan): Pegawai
+    {
+        $user = $this->handleUserUpdated($data);
+
+        $pegawai = $this->getPegawai($data['npk']);
+        $pegawai->update([
+            'nama' => $data['nama'],
+            'npk' => $data['npk'],
+            'golongan_id' => $golonganId,
+            'kode_area' => $data['kode_area'],
+            'area' => $data['area'],
+            'user_id' => $user->id,
+            'is_faktur_ekspedisi' => $data['is_faktur_ekspedisi'],
+            'approval1' => $data['approval1'],
+            'approval2' => $data['approval2'],
+            'approval3' => $data['approval3'],
+            'whatsapp' => $data['whatsapp']
+        ]);
+        
+        $pegawai->jabatan()->sync($jabatan);
+        return $pegawai;
+    }
+
+    /**
+     * update user account
+     * 
+     * @param  array $data
+     * @return  User
+     */
+    private function handleUserUpdated(array $data): User
+    {        
+        $rolesArrayCollect = $this->rolesStringToArray($data['role']);
+
+        $user = $this->getPegawai($data['npk'])->user;
+        $user->update([
+            'name' => $data['nama'],
+            'email' => $data['email']
+        ]);
+        $user->syncRoles($rolesArrayCollect);
 
         return $user;
+    }
+
+    /**
+     * convert roles string to array
+     * 
+     * @param  string  $role
+     * @return  array
+     */
+    private function rolesStringToArray(string $role): array
+    {
+        $rolesArray = explode(',', $role);
+        $rolesArrayCollect = collect();
+
+        foreach ($rolesArray as $role) {
+            $role = trim($role);
+            $rolesArrayCollect->push($role);
+        }
+
+        return $rolesArrayCollect->toArray();
     }
 }
